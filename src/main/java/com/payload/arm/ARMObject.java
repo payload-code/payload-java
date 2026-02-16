bf2777e9877f6106736bf5b840f1a6d07f1cb987
Nathan Yearout <nathan@payload.com>
2026-02-16 17:17:26 -0500

    Improve exception handling in ARMObject.getList - From AI review

--- src/main/java/com/payload/arm/ARMObject.java
+++ src/main/java/com/payload/arm/ARMObject.java
@@ -100,7 +100,7 @@ public class ARMObject<T> {
 				item.setJson(arr.getJSONObject(i));
 				result.add(item);
 			}
-		} catch (Exception exc) {
+		} catch (JSONException | ReflectiveOperationException exc) {
 			// Return empty list on error
 		}
 		return result;
@@ -115,7 +115,7 @@ public class ARMObject<T> {
 				item.setJson(arr.getJSONObject(i));
 				result.add(item);
 			}
-		} catch (Exception exc) {
+		} catch (JSONException | ReflectiveOperationException exc) {
 			// Return empty list on error
 		}
 		return result;
